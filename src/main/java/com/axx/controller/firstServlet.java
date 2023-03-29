404e4aa430919408c12a05eb2065933eac17610b
韩世龙 <slhanf@isoftstone.com>
2023-03-29 17:46:39 +0800

    study

--- src/main/java/com/axx/controller/firstServlet.java
+++ src/main/java/com/axx/controller/firstServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class firstServlet extends HttpServlet {
@@ -14,7 +15,15 @@ public class firstServlet extends HttpServlet {
         System.out.println("哈哈");
         String id = req.getParameter("id");
         System.out.println(id);
+        req.setAttribute("name","张三");
+        //转发
         req.getRequestDispatcher("/index.jsp").forward(req,resp);
+        //服务器端的会话对象   保存会话信息
+        HttpSession session = req.getSession();
+        session.setAttribute("uname","admin");
+        session.setAttribute("upass","admin");
+        //重定向   站外重定向
+        resp.sendRedirect("http:www.baidu.com");
         return;
     }
 
---
+++ src/main/webapp/a.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: issuser
+  Date: 2023/3/29
+  Time: 15:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+2222
+      <p>${name  }</p>
+</body>
+</html>
